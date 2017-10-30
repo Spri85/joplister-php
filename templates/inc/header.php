522fe187fba929013a13a3daaf87a994a571c636
Spri85 <viktorpar@gmail.com>
2017-10-30 14:12:53 +0200

    added templates

---
+++ templates/inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.cs">
+  <title>Joblister</title>
+</head>
+<body>
+<div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="index.php">Home</a></li>
+            <li role="presentation"><a href="create.php">Create Listing</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Joblister</h3>
+      </div>
